7aec67cd940d114189b9df8a6588995134120e48
Katherine Schaumberg <katherine.schaumberg@gmail.com>
2022-03-26 16:53:50 -0500

    add info on making site

--- make_blogdown_site.R
+++ make_blogdown_site.R
@@ -4,3 +4,7 @@ new_site(theme = 'wowchemy/starter-academic')
 blogdown::new_post(title = 'testing', 
                    ext = '.Rmarkdown', 
                    subdir = 'post')
+
+blogdown::check_gitignore()
+
+blogdown::check_content()
